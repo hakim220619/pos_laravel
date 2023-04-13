65138dfddbc4572a0267699ba83f50776934533a
Dani Lukman Hakim <63516526+hakim220619@users.noreply.github.com>
2023-04-13 22:34:01 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
                             </a>
                         </div>
                     </div>
-
+<p>asd</P>
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
